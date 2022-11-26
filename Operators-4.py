5723a3f6389c5927d35a871b5b65782fe419bd55
DemetriosDubis <117493327+DemetriosDubis@users.noreply.github.com>
2022-11-26 16:54:08 -0600

    Python Comparison Operators
    Comparison operators are used to compare two values

---
+++ Operators-4.py
@@ -0,0 +1,7 @@
+Operator	Name	                     Example
+==	      Equal	                     x == y	
+!=	      Not equal	                 x != y	
+>	        Greater than	             x > y	
+<	        Less than	                 x < y	
+>=	      Greater than or equal to	 x >= y	
+<=	      Less than or equal to	     x <= y
