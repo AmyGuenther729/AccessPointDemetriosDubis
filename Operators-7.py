1bd5d2d1b08fa05afb9d898dfc625ed568fb1c39
DemetriosDubis <117493327+DemetriosDubis@users.noreply.github.com>
2022-11-26 17:27:35 -0600

    Python Bitwise Operators
    Bitwise operators are used to compare (binary) numbers

--- Operators-7.py
+++ Operators-7.py
@@ -1,3 +1,7 @@
-Operator	                                       Description	                                        Example
-in 	          Returns True if a sequence with the specified value is present in the object	          x in y	
-not in	      Returns True if a sequence with the specified value is not present in the object	      x not in y
+Operator	    Name	                                 Description
+& 	          AND	                        Sets each bit to 1 if both bits are 1
+|	            OR	                        Sets each bit to 1 if one of two bits is 1
+^	            XOR      	                  Sets each bit to 1 if only one of two bits is 1
+~	            NOT     	                  Inverts all the bits
+<<	          Zero fill left shift	      Shift left by pushing zeros in from the right and let the leftmost bits fall off
+>>	          Signed right shift	        Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
