8434690bf408383513fbed686f9272c699381420
DemetriosDubis <117493327+DemetriosDubis@users.noreply.github.com>
2022-11-26 15:48:53 -0600

    Constructor Funtions

---
+++ Casting-1.py
@@ -0,0 +1,3 @@
+int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
+float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
+str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals
