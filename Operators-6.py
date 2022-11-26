cc3c9185088ec781113068ecd8c85d1ae9969d48
DemetriosDubis <117493327+DemetriosDubis@users.noreply.github.com>
2022-11-26 17:23:35 -0600

    Python Membership Operators
    Membership operators are used to test if a sequence is presented in an object

---
+++ Operators-6.py
@@ -0,0 +1,3 @@
+Operator	                        Description                                 Example
+is 	          Returns True if both variables are the same object	            x is y	
+is not	      Returns True if both variables are not the same object	        x is not y
