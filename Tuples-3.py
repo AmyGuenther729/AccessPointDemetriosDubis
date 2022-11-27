030087dc45485959971fc7392844e95f9d3aadfc
DemetriosDubis <117493327+DemetriosDubis@users.noreply.github.com>
2022-11-26 18:39:55 -0600

    Vocabulary (NO CODE)

---
+++ Tuples-3.py
@@ -0,0 +1,10 @@
+Tuple Items
+Tuple items are ordered, unchangeable, and allow duplicate values.
+
+Tuple items are indexed, the first item has index [0], the second item has index [1] etc.
+
+Ordered
+When we say that tuples are ordered, it means that the items have a defined order, and that order will not change.
+
+Unchangeable
+Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
