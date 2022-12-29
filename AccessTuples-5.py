734fc8180bca43bd3429f976e4d36156e483914f
DemetriosDubis <117493327+DemetriosDubis@users.noreply.github.com>
2022-12-29 13:01:01 -0600

    Range of Negative Indexes

--- AccessTuples-5.py
+++ AccessTuples-5.py
@@ -1,3 +1,2 @@
-tuple1 = ("apple", "banana", "cherry")
-tuple2 = (1, 5, 7, 9, 3)
-tuple3 = (True, False, False)
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
