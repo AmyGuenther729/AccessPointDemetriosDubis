7dbd7ed0064e7e7a9576246346da3fbffce76596
DemetriosDubis <117493327+DemetriosDubis@users.noreply.github.com>
2023-01-02 12:46:32 -0600

    Add Methods
    Add a method called welcome to the Student class:
    
    If you add a method in the child class with the same name as a function in the parent class, the inheritance of the parent method will be overridden.

---
+++ PythonInheritance-9.py
@@ -0,0 +1,7 @@
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
